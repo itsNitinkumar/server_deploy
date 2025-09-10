b85dd8bbda9176d3a8767f3afb8f122ece9bb1e7
Nitin <kumaranish0750@gmail.com>
2025-09-11 00:34:41 +0530

    redeploy testing

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3002;
 
 app.get("/", (_req, res) => {
-  res.send("Hi from HTTP server (TypeScript)!");
+  res.send("Hi from HTTP server (TypeScript)!,, my cicd pipeline");
 });
 
 app.listen(PORT, () => console.log(`HTTP server running on port ${PORT}`));
